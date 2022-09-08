58dbe2846738d1b75251b995a9f5def27152e351
Lucas William Peschke <lucas.peschke@outlook.com.br>
2022-09-08 14:34:56 -0300

    Implementações gerais

--- WebMangaProject/wwwroot/js/site.js
+++ WebMangaProject/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+
+inputBox.onkeyup = (e) => {
+    let userData = e.target.value;
+    let emptyArray = [];
+    if (userData) {
+        emptyArray = suggestions.filter((data) => {
+            return data.toLocaleLowerCase().includes(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data) => {
+            return data = '<li>' + data + '</li>';
+        });
+        console.log(emptyArray);
+        searchWrapper.classList.add("active");
+        showSuggestions(emptyArray);
+    } else {
+        searchWrapper.classList.remove("active");
+    }
+}
+
+function showSuggestions(list) {
+    let listData;
+    if (!list.length) {
+        userValue = inputBox.value;
+        listData = '<li>' + userValue + '</li>';
+    } else if (list.length >= 5) {
+        let maxLength = 5;
+        let newArray = [];
+        for (var i = 0; i < maxLength; i++) {
+            newArray.push(list[i]);
+        }
+        listData = newArray.join('');
+    }
+    else {
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
