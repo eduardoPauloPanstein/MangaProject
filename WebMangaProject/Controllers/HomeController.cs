9dedfac2dd30cee3fe86d29750c713043a880dd8
Júlia <julia.nog.sou@gmail.com>
2022-09-19 17:10:54 -0300

    Implementações no profile

--- WebMangaProject/Controllers/HomeController.cs
+++ WebMangaProject/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace WebMangaProject.Controllers
             return View();
         }
 
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
