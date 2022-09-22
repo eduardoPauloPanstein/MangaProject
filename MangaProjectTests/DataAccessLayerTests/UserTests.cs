d802f4628c900daf3ce3f57b614592221fdef9d6
Júlia <julia.nog.sou@gmail.com>
2022-09-22 17:23:19 -0300

    Alterações no "Profile"

---
+++ MangaProjectTests/DataAccessLayerTests/UserTests.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Implementations;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaProjectTests.DataAccessLayerTests
+{
+    public class UserTests
+    {
+        [Fact]
+        public void GetByFavorites_ReturnNotNull()
+        {
+            using (var context = new MangaProjectDbContext())
+            {
+                UserDAL user = new(context);
+
+                // Act  
+                var u = user.Get(1);
+
+                //Assert  
+                //Assert.NotNull(mangas);
+
+            }
+
+        }
+    }
+}
