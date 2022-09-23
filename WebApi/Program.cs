469eef4bc7984ed8c3cca31b04238165d27d10c4
Paulo Eduardo Panstein <pansteinpaulo@gmail.com>
2022-09-22 21:52:27 -0300

    publish

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -13,8 +13,12 @@ using System.Reflection;
 using System.Text;
 using WebApi;
 using WebApi.Controllers;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 builder.Services.AddControllers();
