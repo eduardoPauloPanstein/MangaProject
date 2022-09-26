8f4bc77efcc5e658f6707ecf3d202d969b6d8502
Paulo Eduardo Panstein <pansteinpaulo@gmail.com>
2022-09-26 13:25:10 -0300

    imp gerais

--- Test/DbFixture.cs
+++ Test/DbFixture.cs
@@ -1,13 +1,8 @@
 ﻿using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MangaProjectTests
+namespace Test
 {
     public class DbFixture
     {
