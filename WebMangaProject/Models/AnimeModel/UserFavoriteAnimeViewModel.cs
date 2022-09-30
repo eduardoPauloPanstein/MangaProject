1163a87238909fc856de6f3b8428c12cb5620801
Júlia <julia.nog.sou@gmail.com>
2022-09-30 17:20:10 -0300

    Alterações na AnimeOnPage,MangaOnPage, AnimeController,MangaController

--- WebMangaProject/Models/AnimeModel/UserFavoriteAnimeViewModel.cs
+++ WebMangaProject/Models/AnimeModel/UserFavoriteAnimeViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Entities.AnimeS;
 using Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace MvcPresentationLayer.Models.AnimeModel
 {
@@ -14,13 +16,20 @@ namespace MvcPresentationLayer.Models.AnimeModel
         public Entities.UserS.User User { get; set; }
 
         public UserMangaStatus Status { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Start Date")]
         public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Finish Date")]
         public DateTime FinishDate { get; set; }
         public MangaScore? Score { get; set; }
+        [DisplayName("Total Rereads")]
         public int? TotalRereads { get; set; }
         public int? Chapter { get; set; }
         public int? Volume { get; set; }
+        [DisplayName("Private Notes")]
         public string? PrivateNotes { get; set; }
+        [DisplayName("Public Notes")]
         public string? PublicNotes { get; set; }
         public bool Private { get; set; }
         public bool Favorite { get; set; }
