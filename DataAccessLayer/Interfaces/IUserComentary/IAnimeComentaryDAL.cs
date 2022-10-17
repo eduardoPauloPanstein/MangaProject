ff551415b9917cee92e3ecbf7c13174ce4416469
Nicolettigf <gfnicoletti@gmail.com>
2022-10-17 15:08:00 -0300

    ok

--- DataAccessLayer/Interfaces/IUserComentary/IAnimeComentaryDAL.cs
+++ DataAccessLayer/Interfaces/IUserComentary/IAnimeComentaryDAL.cs
@@ -7,6 +7,8 @@ namespace DataAccessLayer.Interfaces.IUserComentary
     public interface IAnimeComentaryDAL : ICRUD<AnimeComentary>
     {
         Task<DataResponse<AnimeComentary>> GetByUser(int userid);
+        Task<DataResponse<AnimeComentary>> GetByAnime(int animeID);
+
 
     }
 }
--- WebMangaProject/Apis/MangaProjectApi/ItemComentary/MangaComentary/IMangaProjectApiMangaComentary.cs.cs
+++ WebMangaProject/Apis/MangaProjectApi/ItemComentary/MangaComentary/IMangaProjectApiMangaComentary.cs.cs
@@ -5,6 +5,8 @@ namespace MvcPresentationLayer.Apis.MangaProjectApi.ItemComentary.MangaComentary
     public interface IMangaProjectApiMangaComentary : IMangaProjectApiService<Entities.MangaS.MangaComentary>
     {
         Task<DataResponse<Entities.MangaS.MangaComentary>> GetByUser(int userid);
+        Task<DataResponse<Entities.MangaS.MangaComentary>> GetByManga(int MangaID);
+
 
     }
 }
